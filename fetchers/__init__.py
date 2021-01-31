daae990385f01d2c0d49e70b7af3166f071f7c22
chenyu <believe.chenyu@gmail.com>
2021-01-31 20:27:38 +0800

    add fetcher https://ip.ihuan.me/

--- fetchers/__init__.py
+++ fetchers/__init__.py
@@ -8,6 +8,7 @@ from .GoubanjiaFetcher import GoubanjiaFetcher
 from .IP66Fetcher import IP66Fetcher
 from .IP3366Fetcher import IP3366Fetcher
 from .JiangxianliFetcher import JiangxianliFetcher
+from .IHuanFetcher import IHuanFetcher
 
 fetchers = [
     Fetcher(name='www.kuaidaili.com', fetcher=KuaidailiFetcher),
@@ -15,4 +16,5 @@ fetchers = [
     Fetcher(name='www.66ip.cn', fetcher=IP66Fetcher),
     Fetcher(name='www.ip3366.net', fetcher=IP3366Fetcher),
     Fetcher(name='ip.jiangxianli.com', fetcher=JiangxianliFetcher),
+    Fetcher(name='ip.ihuan.me', fetcher=IHuanFetcher),
 ]
