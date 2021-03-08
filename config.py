ff6e48a3802abf415b03abd5875c8653827e1b11
chenyu <believe.chenyu@gmail.com>
2021-03-08 18:52:31 +0800

    fix a bug

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ VALIDATE_THREAD_NUM = 80 # 验证线程数量
 # 使用代理访问 VALIDATE_URL 网站，超时时间设置为 VALIDATE_TIMEOUT
 # 如果没有超时，并且返回的网页中包含 VALIDATE_TEXT 文字，那么就认为本次验证成功
 # 上述过程最多进行 VALIDATE_MAX_FAILS 次，只要有一次成功，就认为代理可用
-VALIDATE_URL = 'http://www.baidu.com'
+VALIDATE_URL = 'https://www.baidu.com'
 VALIDATE_TEXT = '百度一下，你就知道'
 VALIDATE_TIMEOUT = 8 # 超时时间，单位s
 VALIDATE_MAX_FAILS = 3
